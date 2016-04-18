97f7d072eac28ce87ce23a5022efba229bf1e48d
Pravin <pravin.saindane@infosys.com>
2016-04-18 16:38:22 +0530

    Add Lib test project

--- HelloworldLib/MyLibClass.cs
+++ HelloworldLib/MyLibClass.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace HelloworldLib
 {
@@ -10,7 +6,7 @@ namespace HelloworldLib
     {
         public string GetName()
         {
-            return "MyLibClass";
+            return "HelloworldLib";
         }
 
         public DateTime getDate()
