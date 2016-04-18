66e97e96c843210ff45af4de3f04dbc19a7f8efa
Pravin <pravin.saindane@infosys.com>
2016-04-18 15:27:13 +0530

    Add library project

---
+++ HelloworldLib/MyLibClass.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloworldLib
+{
+    public class MyLibClass
+    {
+        public string GetName()
+        {
+            return "MyLibClass";
+        }
+
+        public DateTime getDate()
+        {
+            return DateTime.Now;
+        }
+    }
+}
