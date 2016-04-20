8d62729c1ba725774f267954f3d2abee1f83c4a9
Pravin <pravin.saindane@infosys.com>
2016-04-20 19:18:54 +0530

    Add nunit tests

---
+++ HelloworldTest/HelloworldTest.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using NUnit.Framework;
+using HelloworldLib;
+
+namespace HelloworldTest
+{
+    [TestFixture]
+    public class HelloworldTest
+    {
+        [Test]
+        public void TestMethodGetName()
+        {
+            MyLibClass classObject = new MyLibClass();
+            string output = classObject.GetName();
+            //string actual = "HelloworldLib";
+            //Assert.AreEqual<string>(actual, output);
+        }
+
+        [Test]
+        public void TestMethodGetDate()
+        {
+            MyLibClass classObject = new MyLibClass();
+            DateTime output = classObject.getDate();
+            //Assert.AreEqual<DateTime>(DateTime.Now, output);
+        }
+    }
+}
